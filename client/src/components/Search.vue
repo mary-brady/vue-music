4edfae189fb7c0ad8eca9a369e93ff5c910e5861
Mary Brady <emceebrady@gmail.com>
2018-09-01 17:03:24 -0600

    home config

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>J A M T O W N </h1>
+    
 </template>
 
 <script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <Search />
+    <h1>J A M T O W N </h1>
+    <form @submit.prevent="search">
+      <input type="text" placeholder="Search for Jams">
+      <button type="submit">Get Jams</button>
+    </form>
+    <Results />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Results from "@/components/Results.vue";
+import Search from "@/components/Search.vue";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      newSearch: {
+        searchQuery: ""
+      }
+    };
+  },
+  computed: {
+    results() {
+      return this.$store.state.Results;
+    }
+  },
+  methods: {
+    search() {
+      this.$store.dispatch("getResults", this.newSearch);
+    }
+  },
+  components: {
+    Results,
+    Search
+  }
+};
+</script>
